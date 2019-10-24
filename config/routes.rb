302d781df82407538dc76ef30dfef21b0bd99d26
Rose McKee <rlnashmckee@gmail.com>
2019-10-24 15:12:28 -0500

    Ecommerce project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post 'add_to_cart' => 'cart#add_to_cart'
+  get 'view_order' => 'cart#view_order'
+  get 'checkout' => 'cart#checkout'
   devise_for :users
   get 'categorical' => 'storefronts#items_by_category'
   get 'branding' => 'storefronts#items_by_brand'
